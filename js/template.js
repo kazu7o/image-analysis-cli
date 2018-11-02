8e62499c72f9e4de3e0e964df3d24c859b809205
Kazuto Mori <kazuto00112@gmail.com>
2018-11-02 12:15:02 +0900

    update 11/2

---
+++ js/template.js
@@ -0,0 +1,15 @@
+const header = (h1) => {
+  let html = "";
+  html += '<header>';
+  html += '<nav>';
+  html += '<ul>';
+  html += '<li><a href="./index.html">ImageAnalyzer</a></li>';
+  html += '<li><a href="./search.html">Flickr API</a></li>';
+  html += '<li><a href="./label.html">CloudVision API</a></li>';
+  html += '<li><a href="./morethan20.html">Flicker.photos.search</a></li>';
+  html += '</ul>';
+  html += '</nav>';
+  html += '<h1>' + h1 + '</h1>';
+  html += '</header>';
+  document.write(html);
+}
